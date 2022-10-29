86c7beeace04e547d3601373f97276095e828cb1
ep1lepsy <veronramkissoon@gmail.com>
2022-10-29 17:06:19 +0000

    added http codes

--- App/views/index.py
+++ App/views/index.py
@@ -20,8 +20,8 @@ def loginAction():
   if user and user.check_password(userdata['password']): # check credentials
     flash('Logged in successfully.') # send message to next page
     login_user(user,True) # login the user
-    return 'User logged in'
-  return 'Invalid credentials'
+    return 'User logged in', 200
+  return 'Invalid credentials', 400
 
 #Allows user to sign in and redirects as needed
 @index_views.route('/signup', methods=['POST'])
@@ -34,8 +34,8 @@ def signup_action():
     db.session.commit() 
   except IntegrityError: 
     db.session.rollback()
-    return 'username already exists'
-  return 'user created'
+    return 'username already exists', 400
+  return 'user created', 201
 
 #Renders the main page with the reviews
 @index_views.route('/reviews', methods=['GET'])
@@ -43,7 +43,7 @@ def signup_action():
 def get_reviews():
     reviews = get_all_reviews()
     reviews = [review.toDict() for review in reviews]
-    return jsonify(reviews)
+    return jsonify(reviews), 200
 
 #Creates a review and should rerender the page
 @index_views.route('/reviews', methods=['POST'])
@@ -55,8 +55,8 @@ def create_review_action():
       return "Student not found"
     result = create_review(review=data['review'],studentId = data['studentId'], userId =current_user.id)
     if result == "pass":
-      return 'Review created'
-    return 'Failed to create review'
+      return 'Review created', 201
+    return 'Failed to create review', 400
 
 #Get a review by an id
 @index_views.route('/reviews/<UserId>', methods=['GET'])
@@ -64,7 +64,7 @@ def create_review_action():
 def get_review_id(UserId):
     reviews = get_review(userId=UserId)
     reviews = [review.toDict() for review in reviews]
-    return jsonify(reviews)
+    return jsonify(reviews), 200
 
 #Create student and should rerender the page
 @index_views.route('/createStudent',methods=['POST'])
@@ -74,15 +74,15 @@ def create_new_student_action():
     student = create_student(id = data['id'], fName = data['firstName'], lName = data['lastName'])
     students = get_all_students()
     if student == "pass":
-      return 'Student Created'
-    return 'Student not created'
+      return 'Student Created', 201
+    return 'Student not created', 400
 
 #Search for student by ID
 @index_views.route('/student/<studentId>',methods=['GET'])
 @login_required
 def search_student(studentId):
   student = get_student(studentId)
-  return student.toDict()
+  return student.toDict(), 200
 
 #update student
 @index_views.route('/student/<id>',methods=['PUT'])
@@ -90,21 +90,21 @@ def search_student(studentId):
 def update_student_by_id(id):
   student = get_student(id)
   if student == None:
-    return 'Student does not exist'
+    return 'Student does not exist', 400
   data = request.get_json()
   update_student(id = data['id'], fName = data['firstName'], lName = data['lastName'])
-  return 'Student updated'
+  return 'Student updated', 200
 
 @index_views.route('/upvote/<id>')
 @login_required
 def upvote(id):
     result = upvote_review(id)
-    return 'pass'
+    return 'Review upvoted', 200
 
 @index_views.route('/downvote/<id>')
 @login_required
 def downvote(id):
     result = downvote_review(id)
-    return 'pass'
+    return 'Review downvoted', 200
 
 
